659155b9cec78faafac0af68e4d2b7c01bcb4f24
Your Name <you@example.com>
2025-04-17 06:54:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# solutiondockercompose
---
+++ compile.sh
@@ -0,0 +1,21 @@
+cd spring-petclinic-admin-server
+mvn clean install
+cd ..
+cd spring-petclinic-api-gateway
+mvn clean install
+cd ..
+cd spring-petclinic-config-server
+mvn clean install
+cd ..
+cd spring-petclinic-customers-service
+mvn clean install
+cd ..
+cd spring-petclinic-discovery-server
+mvn clean install
+cd ..
+cd spring-petclinic-vets-service
+mvn clean install
+cd ..
+cd spring-petclinic-visits-service
+mvn clean install
+cd ..
